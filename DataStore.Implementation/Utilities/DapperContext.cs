b07bfa391d9d59bef771152841475ed9940f0c4f
Keshav Gupta <114326884+guptakeshav1235@users.noreply.github.com>
2024-04-16 11:04:29 +0530

    Update DapperContext.cs

--- DataStore.Implementation/Utilities/DapperContext.cs
+++ DataStore.Implementation/Utilities/DapperContext.cs
@@ -1,4 +1,4 @@
-﻿/*using DataStore.Abstraction.Utilities;
+using DataStore.Abstraction.Utilities;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -8,18 +8,5 @@ namespace DataStore.Implementation.Utilities
     {
         public IDbConnection CreateConnection()
             => new SqlConnection("Server=SS-KESHAVGUPTA\\SQLSERVER;Database=crowdFunding;Trusted_Connection=true");
-    }
-}*/
-
-using DataStore.Abstraction.Utilities;
-using System.Data;
-using System.Data.SqlClient;
-
-namespace DataStore.Implementation.Utilities
-{
-    public class DapperContext : IDapperContext
-    {
-        public IDbConnection CreateConnection()
-            => new SqlConnection("Server=mydatabase.cdwe6smaqz79.ap-south-1.rds.amazonaws.com;Database=crowdFunding;User Id=Keshav;Password=demonslayer123$%#;");
     }
 }
